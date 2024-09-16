10da5cfa16e689d3132419b40eb823d5ff66de94
LucasMilchert <165076729+LucasMilchert@users.noreply.github.com>
2024-09-16 18:15:21 -0300

    Add files via upload

--- DesafioFormulario/index.js
+++ DesafioFormulario/index.js
@@ -1,25 +1,42 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('myForm');
 
     form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Impede o envio padrão do formulário
+a        event.preventDefault(); 
 
-        const name = document.getElementById('name').value.trim();
-        const dob = document.getElementById('dob').value;
 
-        // Validação do nome
+        const nameInput = document.getElementById('name');
+        const dobInput = document.getElementById('dob');
+
+      
+        const name = nameInput.value.trim();
+        const dob = dobInput.value;
+
+      
+        nameInput.setCustomValidity('');
+        dobInput.setCustomValidity('');
+
+        const nameRegex = /^[A-Za-zÀ-ÿ]+$/; 
         if (name === '') {
-            console.log('O campo Nome não pode estar vazio.');
-        } else {
-            console.log('Nome:', name);
+            nameInput.setCustomValidity('O campo Nome não pode estar vazio.');
+        } else if (name.length < 3 || name.length > 120) {
+            nameInput.setCustomValidity('O Nome deve ter entre 3 e 120 caracteres.');
+        } else if (!nameRegex.test(name)) {
+            nameInput.setCustomValidity('O Nome deve conter apenas letras.');
         }
 
-        // Validação da data de nascimento
         if (!dob) {
-            console.log('O campo Data de Nascimento não pode estar vazio.');
-        } else {
+            dobInput.setCustomValidity('O campo Data de Nascimento não pode estar vazio.');
+        }
+
+       
+        if (form.checkValidity()) {
+            console.log('Nome:', name);
             console.log('Data de Nascimento:', dob);
+          
+        } else {
+       
+            console.log('Existem erros no formulário.');
         }
     });
 });
